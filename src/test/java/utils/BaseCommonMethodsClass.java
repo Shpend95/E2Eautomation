2048a2014be25f4226cdf3bc4fd92fac7ec712d6
Shpend95 <shpendpllana5@gmail.com>
2026-02-21 00:54:02 -0500

    deployed app to ec2

--- src/test/java/utils/BaseCommonMethodsClass.java
+++ src/test/java/utils/BaseCommonMethodsClass.java
@@ -39,7 +39,7 @@ public class BaseCommonMethodsClass extends PageInitializer {
                 break;
             case "cloud":
                 appUrl = ConfigReader.read("cloudAppUrl");
-               // remoteUrl = ConfigReader.read("cloudSeleniumHub");
+                remoteUrl = ConfigReader.read("cloudSeleniumHub");
                 break;
             default:
                 throw new IllegalArgumentException("Invalid environment: " + environment);
@@ -56,8 +56,7 @@ public class BaseCommonMethodsClass extends PageInitializer {
 
         switch (browser.toLowerCase()) {
             case "chrome":
-               // if (environment.equalsIgnoreCase("local")) {
-                if (environment.equalsIgnoreCase("local") || environment.equalsIgnoreCase("cloud")) {
+                if (environment.equalsIgnoreCase("local")) {
                     // LOCAL: Run Chrome on your machine
                     ChromeOptions localOptions = new ChromeOptions();
                     if (headless) {
