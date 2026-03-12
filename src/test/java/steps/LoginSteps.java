8c0fa3561755475abc2b6a6b562ea2be2eef91c5
Shpend95 <shpendpllana5@gmail.com>
2026-03-12 16:29:43 -0400

    D

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -8,40 +8,39 @@ import org.openqa.selenium.WebElement;
 import utils.BaseCommonMethodsClass;
 import utils.Log;
 
+
 public class LoginSteps extends BaseCommonMethodsClass {
-
-   String username="";
-
+    String username = "";
 
     @Given("patient is navigated to the website")
     public void patient_is_navigated_to_the_website() throws Exception {
-      launchBrowser();
 
     }
+
     @When("patient enters valid email and password")
     public void patient_enters_valid_email_and_password() throws InterruptedException {
         loginPage.userName.sendKeys("hrm_user");
-        username=loginPage.userName.getText();
+        username = loginPage.userName.getText();
         loginPage.passWord.clear();
         loginPage.passWord.sendKeys("Hrm_user@123");
 
     }
+
     @When("patient clicks on login button")
     public void patient_clicks_on_login_button() throws InterruptedException {
         loginPage.signBtn.click();
         Log.info("logged in successfully");
 
 
-
     }
+
     @Then("patient is successfully logged in")
     public void patient_is_successfully_logged_in() {
 
-        WebElement dashboardMessage=driver.findElement(By.xpath("//h6[text()='Dashboard']"));
-        if(dashboardMessage.isDisplayed()){
+        WebElement dashboardMessage = driver.findElement(By.xpath("//h6[text()='Dashboard']"));
+        if (dashboardMessage.isDisplayed()) {
             System.out.println("Congrats, your are logged in");
         }
-
 
 
     }
